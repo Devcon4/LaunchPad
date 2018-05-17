959dc11e603c96b8c7349e02bfbe9afb66bc65f6
Devcon4 <devcon42@gmail.com>
2018-05-16 21:07:51 -0600

    Init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, RoutesRecognized, Route } from '@angular/router';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '*',
+    redirectTo: 'profile'
+  },
+  {
+    path: 'profile',
+    component: UserProfileComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

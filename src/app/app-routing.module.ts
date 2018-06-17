ac239d8a55c69635bd0ca78da4f8759e2d86d0b7
Devcon4 <devcon42@gmail.com>
2018-06-16 22:36:22 -0600

    remove login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, RoutesRecognized, Route } from '@angular/router';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
-import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
@@ -9,10 +8,6 @@ const routes: Routes = [
     path: '',
     redirectTo: 'login',
     pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    component: LoginComponent
   },
   {
     path: 'profile/:id',
